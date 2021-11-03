69eb153bffa459b1b1b2eb20d566f89c872224ca
Diana Udisteanu <diana.udisteanu@yahoo.com>
2021-11-03 18:03:59 +0200

    Add build folder to store binaries.

---
+++ build/CMakeFiles/vehicul.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vehicul.dir/src/main.cpp.o"
+  "CMakeFiles/vehicul.dir/src/main.cpp.o.d"
+  "vehicul"
+  "vehicul.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vehicul.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/vehicul.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for vehicul.
+# This may be replaced when dependencies are built.
